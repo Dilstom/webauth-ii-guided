58d89ffc5a847551dde6207b622597be9442006a
Dil Sombayeva <dilyasom@gmail.com>
2019-05-09 21:51:40 -0700

    save session

--- api/server.js
+++ api/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session'); // 1.add this
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
+
+// 3.add this
+const sessionConfig = {
+ name: 'monkey',
+ secret: 'keep it secret, keep it safe',
+ cookie: {
+  maxAge: 1000 * 60 * 10, // in ms available (15 min)
+  //   maxAge: 1000 * 60 * 60 * 24, // in ms available (24 hours)
+  secure: false, // used over http only 'false' for development
+  httpOnly: true, // cannot access the cookie
+ },
+ // can the user access the cookie from js using document.cookie
+ resave: false, //
+ saveUninitialized: false, // GDPR laws against setting cookies automatically
+};
+
+server.use(session(sessionConfig)); // 2. add this
 
 server.use(helmet());
 server.use(express.json());
@@ -15,7 +33,7 @@ server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+ res.send("It's alive!");
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,36 +5,50 @@ const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+ let user = req.body;
+ const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+ user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+ Users.add(user)
+  .then(saved => {
+   res.status(201).json(saved);
+  })
+  .catch(error => {
+   res.status(500).json(error);
+  });
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+ let { username, password } = req.body;
+ //  console.log(bcrypt);
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
+ Users.findBy({ username })
+  .first()
+  .then(user => {
+   if (user && bcrypt.compareSync(password, user.password)) {
+    // 4. add this to save it on session
+    req.session.user = user;
+    res.status(200).json({
+     message: `Welcome ${user.username}! , have a cookie`,
     });
+   } else {
+    res.status(401).json({ message: 'Invalid Credentials' });
+   }
+  })
+  .catch(error => {
+   res.status(500).json(error);
+  });
+});
+
+server.get('/api/logout', (req, res) => {
+ if (req.session) {
+  req.session.destroy(err => {
+   if (err) {
+   } else {
+   }
+  });
+ } else {
+ }
 });
 
 module.exports = router;
