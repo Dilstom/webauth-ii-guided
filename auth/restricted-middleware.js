e3287e2d2cff5edd188142ac97f0554ad04ddd2f
Dil Sombayeva <dilyasom@gmail.com>
2019-05-13 12:14:09 -0700

    add restricted-middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,3 +1,21 @@
+const jwt = require('jsonwebtoken');
+const secret = require('./secrets');
+
 module.exports = (req, res, next) => {
- next();
+ const token = req.headers.authorization;
+ if (token) {
+  jwt.verify(token, secret.secretJwt, (err, decodedToken) => {
+   if (err) {
+    res
+     .status(401)
+     .json({ message: 'You shall not pass - the token is wrong!' });
+   } else {
+    next();
+   }
+  });
+ } else {
+  res
+   .status(401)
+   .json({ message: 'You shall not pass. You are not authenticated' });
+ }
 };
