f9540c1a38ccce63adf4bec1f267dddb81a4ffed
Dil Sombayeva <dilyasom@gmail.com>
2019-05-13 12:13:45 -0700

    add a function to create token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
-
+const secret = require('./secrets').secretJwt;
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
  let user = req.body;
@@ -18,6 +19,18 @@ router.post('/register', (req, res) => {
   });
 });
 
+function generateToken(user) {
+ const payload = {
+  subject: user.id, // sub in pauload is what the token is about
+  username: user.username,
+  // ...otherData
+ };
+ const options = {
+  expiresIn: '1d',
+ };
+ return jwt.sign(payload, secret, options);
+}
+
 router.post('/login', (req, res) => {
  let { username, password } = req.body;
  //  console.log(bcrypt);
@@ -26,8 +39,12 @@ router.post('/login', (req, res) => {
   .first()
   .then(user => {
    if (user && bcrypt.compareSync(password, user.password)) {
+    // 1. generate token
+    const token = generateToken(user);
     res.status(200).json({
-     message: `Welcome ${user.username}! , have a cookie`,
+     // send token back
+     message: `Welcome ${user.username}! Here is your token`,
+     token,
     });
    } else {
     res.status(401).json({ message: 'Invalid Credentials' });
